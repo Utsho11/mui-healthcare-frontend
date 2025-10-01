b715007c570f880dc493c8e16b78e8e401c354b6
Utsho11 <utshoroy521@gmail.com>
2025-10-02 01:40:15 +0600

    Top rated doctors section added in homepage

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "@/components/UI/Homepage/HeroSection/HeroSection";
 import Specialist from "@/components/UI/Homepage/Specialist/Specialist";
+import TopRatedDoctor from "@/components/UI/Homepage/TopRatedDoctor/TopRatedDoctor";
 
 export default function HomePage() {
   return (
     <div className="">
       <HeroSection />
       <Specialist />
+      <TopRatedDoctor />
     </div>
   );
 }
---
+++ src/components/UI/Homepage/TopRatedDoctor/TopRatedDoctor.tsx
@@ -0,0 +1,122 @@
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Container,
+  Grid,
+  Typography,
+} from "@mui/material";
+import PlaceOutlinedIcon from "@mui/icons-material/PlaceOutlined";
+
+const TopRatedDoctor = async () => {
+  const res = await fetch("http://localhost:5000/api/v1/doctor?page=1&limit=3");
+  const { data: doctors } = await res.json();
+
+  //   console.log(doctors);
+
+  return (
+    <Box
+      sx={{
+        my: 10,
+        py: 30,
+        backgroundColor: "rgb(20,20,20,0.1)",
+        clipPath: "polygon(0 0,100% 25%, 100% 100%, 0% 75%)",
+      }}
+    >
+      <Box
+        sx={{
+          textAlign: "center",
+        }}
+      >
+        <Typography variant="h4" component="h1" fontWeight={600}>
+          Our Top Rated Doctors
+        </Typography>
+        <Typography
+          mt={4}
+          component="p"
+          fontWeight={300}
+          fontSize={18}
+          color="secondary.main"
+        >
+          Access to expert physicians and surgeons, advanced technologies
+        </Typography>
+        <Typography
+          component="p"
+          fontWeight={300}
+          fontSize={18}
+          color="secondary.main"
+        >
+          and top-quality surgery facilities right here.
+        </Typography>
+      </Box>
+      <Container
+        sx={{
+          margin: "30px auto",
+        }}
+      >
+        <Grid container spacing={2}>
+          {doctors.map((doctor: any) => (
+            <Grid item key={doctor.id} size={{ md: 4 }}>
+              <Card sx={{ maxWidth: 345 }}>
+                <CardMedia
+                  sx={{ height: 240 }}
+                  image={doctor.profilePhoto}
+                  title="green iguana"
+                />
+                <CardContent>
+                  <Typography
+                    gutterBottom
+                    variant="h5"
+                    component="h2"
+                    fontWeight={600}
+                  >
+                    {doctor.name}
+                  </Typography>
+                  <Typography variant="body2" sx={{ color: "text.secondary" }}>
+                    {doctor.qualification}, {doctor.designation}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    sx={{ color: "text.secondary" }}
+                    mt={2}
+                  >
+                    <PlaceOutlinedIcon fontSize="small" /> {doctor.address}
+                  </Typography>
+                </CardContent>
+                <CardActions
+                  sx={{
+                    justifyContent: "space-between",
+                    px: 2,
+                    paddingBottom: "20px",
+                  }}
+                >
+                  <Button>Book Now</Button>
+                  <Button variant="outlined">View Profile</Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+        <Box
+          sx={{
+            textAlign: "center",
+          }}
+        >
+          <Button
+            variant="outlined"
+            sx={{
+              marginTop: "20px",
+            }}
+          >
+            View ALL
+          </Button>
+        </Box>
+      </Container>
+    </Box>
+  );
+};
+
+export default TopRatedDoctor;
