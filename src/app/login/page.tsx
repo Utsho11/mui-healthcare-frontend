95ede0af9030169f9b8a865aa49cd29183899827
Utsho11 <utshoroy521@gmail.com>
2025-10-03 00:47:24 +0600

    add user login using server action function

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   Box,
   Button,
@@ -10,8 +11,33 @@ import {
 import Image from "next/image";
 import assets from "@/assets";
 import Link from "next/link";
+import { toast } from "sonner";
+import { useForm, type SubmitHandler } from "react-hook-form";
+import { userLogin } from "@/services/action/userLogin";
+import { useRouter } from "next/navigation";
+
+export interface IFormValue {
+  email: string;
+  password: string;
+}
 
 const LoginPage = () => {
+  const router = useRouter();
+  const { register, handleSubmit } = useForm<IFormValue>();
+  const onSubmit: SubmitHandler<IFormValue> = async (values) => {
+    try {
+      // console.log(values);
+      const res = await userLogin(values);
+      if (res?.data?.accessToken) {
+        toast.success(res?.message);
+        router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    // console.log(data.get("data"));
+  };
+
   return (
     <Container>
       <Stack
@@ -46,54 +72,58 @@ const LoginPage = () => {
               </Typography>
             </Box>
           </Stack>
-          <Grid container my={1}>
-            <Grid
-              size={{ md: 12 }}
-              my={1}
-              gap={2}
-              sx={{
-                display: "flex",
-              }}
-            >
-              <Grid size={{ md: 6 }}>
-                <TextField
-                  label="Email"
-                  variant="outlined"
-                  size="small"
-                  fullWidth={true}
-                />
-              </Grid>
-              <Grid size={{ md: 6 }}>
-                <TextField
-                  label="Password"
-                  type="password"
-                  variant="outlined"
-                  size="small"
-                  fullWidth={true}
-                />
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Grid container my={1}>
+              <Grid
+                size={{ md: 12 }}
+                my={1}
+                gap={2}
+                sx={{
+                  display: "flex",
+                }}
+              >
+                <Grid size={{ md: 6 }}>
+                  <TextField
+                    label="Email"
+                    variant="outlined"
+                    size="small"
+                    fullWidth={true}
+                    {...register("email")}
+                  />
+                </Grid>
+                <Grid size={{ md: 6 }}>
+                  <TextField
+                    label="Password"
+                    type="password"
+                    variant="outlined"
+                    size="small"
+                    fullWidth={true}
+                    {...register("password")}
+                  />
+                </Grid>
               </Grid>
             </Grid>
-          </Grid>
-          <Link href="/">
-            <Typography color="secondary.main" textAlign="end" mb={1}>
-              Forgot Password?
+            <Link href="/">
+              <Typography color="secondary.main" textAlign="end" mb={1}>
+                Forgot Password?
+              </Typography>
+            </Link>
+            <Button
+              sx={{
+                margin: "10px 0px",
+              }}
+              fullWidth={true}
+              type="submit"
+            >
+              LOGIN
+            </Button>
+            <Typography component="p" fontWeight={300}>
+              Don&apos;t have an account?{" "}
+              <Typography component="span" color="primary">
+                <Link href="/register">Create an account</Link>
+              </Typography>
             </Typography>
-          </Link>
-          <Button
-            sx={{
-              margin: "10px 0px",
-            }}
-            fullWidth={true}
-            type="submit"
-          >
-            LOGIN
-          </Button>
-          <Typography component="p" fontWeight={300}>
-            Don&apos;t have an account?{" "}
-            <Typography component="span" color="primary">
-              <Link href="/register">Create an account</Link>
-            </Typography>
-          </Typography>
+          </form>
         </Box>
       </Stack>
     </Container>
