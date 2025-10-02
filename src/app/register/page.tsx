291b86e79339c18595e13fd7225e70f58326ec03
Utsho11 <utshoroy521@gmail.com>
2025-10-02 20:52:51 +0600

    add modify patientdata into formdata

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   Box,
   Button,
@@ -10,8 +11,28 @@ import {
 import Image from "next/image";
 import assets from "@/assets";
 import Link from "next/link";
+import { useForm, SubmitHandler } from "react-hook-form";
+import modifyPayload from "@/utils/modifyPayload";
+
+interface IPatientData {
+  name: string;
+  email: string;
+  contactNumber: string;
+  address: string;
+}
+
+interface IPatientFormData {
+  password: string;
+  patient: IPatientData;
+}
 
 const RegisterPage = () => {
+  const { register, handleSubmit } = useForm<IPatientFormData>();
+  const onSubmit: SubmitHandler<IPatientFormData> = (values) => {
+    const data = modifyPayload(values);
+    // console.log(data.get("data"));
+  };
+
   return (
     <Container>
       <Stack
@@ -47,83 +68,90 @@ const RegisterPage = () => {
             </Box>
           </Stack>
           <Box>
-            <Grid container my={1}>
-              <Grid size={{ md: 12 }} my={1}>
-                <TextField
-                  label="Name"
-                  variant="outlined"
-                  size="small"
-                  fullWidth={true}
-                />
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <Grid container my={1}>
+                <Grid size={{ md: 12 }} my={1}>
+                  <TextField
+                    label="Name"
+                    variant="outlined"
+                    size="small"
+                    fullWidth={true}
+                    {...register("patient.name")}
+                  />
+                </Grid>
+                <Grid
+                  size={{ md: 12 }}
+                  my={1}
+                  gap={2}
+                  sx={{
+                    display: "flex",
+                  }}
+                >
+                  <Grid size={{ md: 6 }}>
+                    <TextField
+                      label="Email"
+                      variant="outlined"
+                      size="small"
+                      fullWidth={true}
+                      {...register("patient.email")}
+                    />
+                  </Grid>
+                  <Grid size={{ md: 6 }}>
+                    <TextField
+                      label="Password"
+                      variant="outlined"
+                      size="small"
+                      fullWidth={true}
+                      {...register("password")}
+                    />
+                  </Grid>
+                </Grid>
+                <Grid
+                  size={{ md: 12 }}
+                  my={1}
+                  gap={2}
+                  sx={{
+                    display: "flex",
+                  }}
+                >
+                  <Grid size={{ md: 6 }}>
+                    <TextField
+                      label="Contact Number"
+                      variant="outlined"
+                      size="small"
+                      fullWidth={true}
+                      {...register("patient.contactNumber")}
+                    />
+                  </Grid>
+                  <Grid size={{ md: 6 }}>
+                    <TextField
+                      label="Address"
+                      variant="outlined"
+                      size="small"
+                      fullWidth={true}
+                      {...register("patient.address")}
+                    />
+                  </Grid>
+                </Grid>
               </Grid>
-              <Grid
-                size={{ md: 12 }}
-                my={1}
-                gap={2}
+              <Button
                 sx={{
-                  display: "flex",
+                  margin: "10px 0px",
                 }}
+                fullWidth={true}
+                type="submit"
               >
-                <Grid size={{ md: 6 }}>
-                  <TextField
-                    label="Email"
-                    variant="outlined"
-                    size="small"
-                    fullWidth={true}
-                  />
-                </Grid>
-                <Grid size={{ md: 6 }}>
-                  <TextField
-                    label="Password"
-                    variant="outlined"
-                    size="small"
-                    fullWidth={true}
-                  />
-                </Grid>
-              </Grid>
-              <Grid
-                size={{ md: 12 }}
-                my={1}
-                gap={2}
-                sx={{
-                  display: "flex",
-                }}
-              >
-                <Grid size={{ md: 6 }}>
-                  <TextField
-                    label="Contact Number"
-                    variant="outlined"
-                    size="small"
-                    fullWidth={true}
-                  />
-                </Grid>
-                <Grid size={{ md: 6 }}>
-                  <TextField
-                    label="Address"
-                    variant="outlined"
-                    size="small"
-                    fullWidth={true}
-                  />
-                </Grid>
-              </Grid>
-            </Grid>
-            <Button
-              sx={{
-                margin: "10px 0px",
-              }}
-              fullWidth={true}
-              type="submit"
-            >
-              REGISTER
-            </Button>
-            <Typography component="p" fontWeight={300}>
-              Do you already have an account?{" "}
-              <Link href="/login">
-                <Typography component="span" color="primary">
-                  Login
-                </Typography>
-              </Link>
-            </Typography>
+                REGISTER
+              </Button>
+              <Typography component="p" fontWeight={300}>
+                Do you already have an account?{" "}
+                <Link href="/login">
+                  <Typography component="span" color="primary">
+                    Login
+                  </Typography>
+                </Link>
+              </Typography>
+            </form>
           </Box>
         </Box>
       </Stack>
---
+++ src/utils/modifyPayload.ts
@@ -0,0 +1,9 @@
+const modifyPayload = (values: any) => {
+  const obj = { ...values };
+  const data = JSON.stringify(obj);
+  const formData = new FormData();
+  formData.append("data", data);
+  return formData;
+};
+
+export default modifyPayload;
