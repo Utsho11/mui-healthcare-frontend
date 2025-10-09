7d0b75d5223bbe7485af2f0d8b51026421a43fcd
Utsho11 <utshoroy521@gmail.com>
2025-10-09 23:44:14 +0600

    specialties table added

--- src/app/(withDasboardLayout)/dashboard/admin/specialties/page.tsx
+++ src/app/(withDasboardLayout)/dashboard/admin/specialties/page.tsx
@@ -1,15 +1,63 @@
 "use client";
-import { Box, Button, Stack, TextField, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  IconButton,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { useState } from "react";
 import SpecialityModal from "./component/SpecialityModal";
 import { useGaetAllSpecialityQuery } from "@/redux/api/specialitiesApi";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import Image from "next/image";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 const SpecialtiesPage = () => {
   const [isModalOpen, setModalOpen] = useState<boolean>(false);
 
+  const columns: GridColDef[] = [
+    { field: "title", headerName: "Title", width: 300 },
+    {
+      field: "icon",
+      headerName: "Icon",
+      width: 300,
+
+      renderCell: ({ row }) => {
+        return (
+          <Box
+            sx={{
+              py: 2,
+            }}
+          >
+            <Image src={row.icon} width={20} height={20} alt="icon" />
+          </Box>
+        );
+      },
+    },
+    {
+      field: "action",
+      headerName: "Action",
+      width: 400,
+      renderCell: ({ row }) => {
+        return (
+          <IconButton aria-label="delete" onClick={() => handleDelete(row.id)}>
+            <DeleteIcon />
+          </IconButton>
+        );
+      },
+    },
+  ];
+
   const { data } = useGaetAllSpecialityQuery({});
 
-  console.log(data);
+  // console.log(data);
+
+  const handleDelete = (id: string) => {
+    console.log(id);
+  };
+
   return (
     <Box>
       <Stack direction="row" alignItems="center" justifyContent="space-between">
@@ -19,6 +67,7 @@ const SpecialtiesPage = () => {
       </Stack>
       <Box>
         <Typography>All Specialities</Typography>
+        <DataGrid rows={data} columns={columns} />
       </Box>
     </Box>
   );
