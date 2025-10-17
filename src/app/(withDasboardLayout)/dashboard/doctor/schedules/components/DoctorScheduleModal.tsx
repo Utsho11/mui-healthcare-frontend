5dea37d8edbf74118c14dd524dfafeac1c475907
Utsho11 <utshoroy521@gmail.com>
2025-10-18 01:26:12 +0600

    create doctor schedule added

--- src/app/(withDasboardLayout)/dashboard/doctor/schedules/components/DoctorScheduleModal.tsx
+++ src/app/(withDasboardLayout)/dashboard/doctor/schedules/components/DoctorScheduleModal.tsx
@@ -7,6 +7,10 @@ import { useState } from "react";
 import dayjs from "dayjs";
 import { useGetAllScheduleQuery } from "@/redux/api/scheduleApi";
 import MultipleSelectedFieldChip from "./MultiSelectedFieldChip";
+import LoadingButton from "@mui/lab/LoadingButton";
+import { Stack } from "@mui/material";
+import { useCreateDoctorScheduleMutation } from "@/redux/api/doctorScheduleApi";
+import { toast } from "sonner";
 
 type TProps = {
   open: boolean;
@@ -37,23 +41,53 @@ const DoctorScheduleModal = ({ open, setOpen }: TProps) => {
   const { data } = useGetAllScheduleQuery(query);
   const schedules = data?.schedules;
 
+  const [createDoctorSchedule, { isLoading }] =
+    useCreateDoctorScheduleMutation();
+
+  console.log(selectedScheduleIds);
+
+  const onSubmit = async () => {
+    try {
+      const res = await createDoctorSchedule({
+        scheduleIds: selectedScheduleIds,
+      }).unwrap();
+      if (res.count > 0) {
+        toast.success("Doctor Schedule created Successfully.");
+      }
+      setOpen(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <MUIModal open={open} setOpen={setOpen} title="Create Doctor Schedule">
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <DatePicker
-          label="Controlled picker"
-          value={dayjs(selectedDate)}
-          onChange={(newValue) =>
-            setSelectedDate(dayjs(newValue).toISOString())
-          }
-          sx={{ width: "100%" }}
+      <Stack direction="column" gap={2}>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DatePicker
+            label="Controlled picker"
+            value={dayjs(selectedDate)}
+            onChange={(newValue) =>
+              setSelectedDate(dayjs(newValue).toISOString())
+            }
+            sx={{ width: "100%" }}
+          />
+        </LocalizationProvider>
+        <MultipleSelectedFieldChip
+          schedules={schedules}
+          selectedScheduleIds={selectedScheduleIds}
+          setSelectedScheduleIds={setSelectedScheduleIds}
         />
-      </LocalizationProvider>
-      <MultipleSelectedFieldChip
-        schedules={schedules}
-        selectedScheduleIds={selectedScheduleIds}
-        setSelectedScheduleIds={setSelectedScheduleIds}
-      />
+        <LoadingButton
+          size="small"
+          onClick={onSubmit}
+          loading={isLoading}
+          loadingIndicator="Submitting..."
+          variant="contained"
+        >
+          <span>Submit</span>
+        </LoadingButton>
+      </Stack>
     </MUIModal>
   );
 };
