777316e9b67bed69e121bd5200cd62547daa87cd
Utsho11 <utshoroy521@gmail.com>
2025-10-19 21:26:41 +0600

    showing profile info

--- src/app/(withDasboardLayout)/dashboard/doctor/profile/page.tsx
+++ src/app/(withDasboardLayout)/dashboard/doctor/profile/page.tsx
@@ -1,8 +1,22 @@
+"use client";
+import { useGetMYProfileQuery } from "@/redux/api/myProfile";
+import { Container, Grid } from "@mui/material";
+import DoctorInformation from "./components/DoctorInformation";
 
 const Profile = () => {
+  const { data, isLoading } = useGetMYProfileQuery({});
+  if (isLoading) {
+    <p>Loading...</p>;
+  }
   return (
-    <div>Doctor Profile</div>
-  )
-}
+    <>
+      <Container>
+        <Grid size={{ xs: 12, md: 8 }}>
+          <DoctorInformation data={data} />
+        </Grid>
+      </Container>
+    </>
+  );
+};
 
-export default Profile
+export default Profile;
