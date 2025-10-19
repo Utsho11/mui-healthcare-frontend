6ef2a5f1539e378bd3a476a474bef01d4410a8a3
Utsho11 <utshoroy521@gmail.com>
2025-10-19 21:36:14 +0600

    changing doctor profile image

--- src/app/(withDasboardLayout)/dashboard/doctor/profile/page.tsx
+++ src/app/(withDasboardLayout)/dashboard/doctor/profile/page.tsx
@@ -1,18 +1,67 @@
 "use client";
-import { useGetMYProfileQuery } from "@/redux/api/myProfile";
-import { Container, Grid } from "@mui/material";
+import {
+  useGetMYProfileQuery,
+  useUpdateMYProfileMutation,
+} from "@/redux/api/myProfile";
+import { Box, Container, Grid } from "@mui/material";
 import DoctorInformation from "./components/DoctorInformation";
+import Image from "next/image";
+import AutoFileUploader from "@/components/Form/AutoFileUploader";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 
 const Profile = () => {
   const { data, isLoading } = useGetMYProfileQuery({});
+
+  const [updateMYProfile, { isLoading: updating }] =
+    useUpdateMYProfileMutation();
+
+  const fileUploadHandler = (file: File) => {
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("data", JSON.stringify({}));
+
+    updateMYProfile(formData);
+  };
   if (isLoading) {
     <p>Loading...</p>;
   }
   return (
     <>
       <Container>
-        <Grid size={{ xs: 12, md: 8 }}>
-          <DoctorInformation data={data} />
+        <Grid container spacing={4}>
+          <Grid size={{ xs: 12, md: 8 }}>
+            <Box
+              sx={{
+                height: 300,
+                width: "100%",
+                overflow: "hidden",
+                borderRadius: 1,
+              }}
+            >
+              <Image
+                height={300}
+                width={400}
+                src={data?.profilePhoto}
+                alt="User Photo"
+              />
+            </Box>
+            <Box my={3}>
+              {updating ? (
+                <p>Uploading...</p>
+              ) : (
+                <AutoFileUploader
+                  name="file"
+                  label="Choose Your Profile Photo"
+                  icon={<CloudUploadIcon />}
+                  onFileUpload={fileUploadHandler}
+                  variant="text"
+                />
+              )}
+            </Box>
+          </Grid>
+          <Grid size={{ xs: 12, md: 8 }}>
+            <DoctorInformation data={data} />
+          </Grid>
         </Grid>
       </Container>
     </>
